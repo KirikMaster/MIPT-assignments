7b9b95eca9f42cd1d13d783c049c5fc1ec0904d2
KirikMaster <62561802+KirikMaster@users.noreply.github.com>
2020-04-15 17:51:20 +0300

    Update 1.cpp

--- 2020/06 - Templates/1.cpp
+++ 2020/06 - Templates/1.cpp
@@ -1,20 +1,5 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
 template<typename T>
 T get_min_value(T& a, T& b) {
     if (a < b) return a;
     else return b;
-}
-
-int main() {
-    int a = 5;
-    int b = 4;
-    string s1 = "abc";
-    string s2 = "zxc";
-    cout << "Min int: " << get_min_value(a, b) << endl;
-    cout << "Min string: " << get_min_value(s1, s2) << endl;
-    return 0;
 }
