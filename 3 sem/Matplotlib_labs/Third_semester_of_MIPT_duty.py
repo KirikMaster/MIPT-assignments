5e6a9e6d7542e8aa71817e83a522cf4928e92dd4
KirikMaster <62561802+KirikMaster@users.noreply.github.com>
2020-12-01 16:29:58 +0300

    Update Third_semester_of_MIPT_duty.py

--- 3 sem/Matplotlib_labs/Third_semester_of_MIPT_duty.py
+++ 3 sem/Matplotlib_labs/Third_semester_of_MIPT_duty.py
@@ -1,16 +1,13 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.animation import ArtistAnimation
 import requests
 
 url = requests.get('http://judge2.vdi.mipt.ru/ejudge/lab_01_mpl/frames.dat')
 Lines = url.text.split('\n')
 N = len(Lines)//2
 X, Y = [[float(j) for j in Lines[2*i].split()] for i in range(N)], [[float(j) for j in Lines[2*i + 1].split()] for i in range(N)]
-
-fig = plt.figure()
-
 frames = []
-#trans = plt.axes().transAxes
+fig = plt.figure()
 
 for i in range(N):
     frame, = plt.plot(X[i], Y[i], 'r-')
@@ -20,6 +17,5 @@ miny, maxy = min(Y[5]) - 0.1 * abs(max(Y[5]) - min(Y[5])), max(Y[5]) + 0.1 * abs
 plt.axis([min(X[0]), max(X[0]), miny, maxy])
 plt.grid(True)
 
-#ani = animation.ArtistAnimation(fig, lns, interval=50)
-anim = animation.ArtistAnimation(fig, frames, interval=200)
+anim = ArtistAnimation(fig, frames, interval=200)
 plt.show()
