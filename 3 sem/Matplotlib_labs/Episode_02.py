da731c4a9cf7c1cf3bb2e7040c023d03aa1003e0
KirikMaster <62561802+KirikMaster@users.noreply.github.com>
2020-12-01 17:29:39 +0300

    Add files via upload

--- 3 sem/Matplotlib_labs/Episode_02.py
+++ 3 sem/Matplotlib_labs/Episode_02.py
@@ -7,17 +7,19 @@ Lines = url.text.split('\n')
 N = len(Lines)//2
 X, Y = [[float(j) for j in Lines[2*i].split()] for i in range(N)], [[float(j) for j in Lines[2*i + 1].split()] for i in range(N)]
 frames = []
-fig = plt.figure
-ax = plt.axes
-#print(len(X), X[0][1], X[1][1], X[2][1], X[3][1], X[4][1], X[5][1], sep='\n')
-#print(len(Y), Y[0][1], Y[1][1], Y[2][1], Y[3][1], Y[4][1], Y[5][1], sep='\n')
+fig = plt.figure()
 
 for i in range(N):
-    ln, = plt.plot(X[i], Y[i])
-    frames.append([ln])
-miny, maxy = min(Y[0]) - 0.1 * abs(max(Y[0]) - min(Y[0])), max(Y[0]) + 0.1 * abs(max(Y[0]) - min(Y[0]))
+    frame, = plt.plot(X[i], Y[i], 'r-')
+    tx = plt.text(0.5, 10, "frame " + str(i+1), fontsize = 16, c = '#FF5500')
+    frames.append([frame,tx])
+
+miny, maxy = min(Y[5]) - 0.1 * abs(max(Y[5]) - min(Y[5])), max(Y[5]) + 0.1 * abs(max(Y[5]) - min(Y[5]))
+plt.title("f(x)")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.axis([min(X[0]), max(X[0]), miny, maxy])
 plt.grid(True)
 
-anim = ArtistAnimation(fig, frames, interval = 300, repeat = True, repeat_delay = 1000)
+anim = ArtistAnimation(fig, frames, interval=200)
 plt.show()
