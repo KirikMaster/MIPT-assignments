fdae27afb55ca27370b645a33975d4bfcc543729
KirikMaster <62561802+KirikMaster@users.noreply.github.com>
2020-12-01 16:02:25 +0300

    Add files via upload

--- 3 sem/Matplotlib_labs/Episode_02.py
+++ 3 sem/Matplotlib_labs/Episode_02.py
@@ -13,11 +13,11 @@ ax = plt.axes
 #print(len(Y), Y[0][1], Y[1][1], Y[2][1], Y[3][1], Y[4][1], Y[5][1], sep='\n')
 
 for i in range(N):
-    frames.append([plt.imshow(X[i], Y[i])])
+    ln, = plt.plot(X[i], Y[i])
+    frames.append([ln])
 miny, maxy = min(Y[0]) - 0.1 * abs(max(Y[0]) - min(Y[0])), max(Y[0]) + 0.1 * abs(max(Y[0]) - min(Y[0]))
 plt.axis([min(X[0]), max(X[0]), miny, maxy])
 plt.grid(True)
 
 anim = ArtistAnimation(fig, frames, interval = 300, repeat = True, repeat_delay = 1000)
-
 plt.show()
--- 3 sem/Matplotlib_labs/Third_semester_of_MIPT_duty.py
+++ 3 sem/Matplotlib_labs/Third_semester_of_MIPT_duty.py
@@ -1,42 +1,25 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import requests
-
-fig = plt.figure()
 
 url = requests.get('http://judge2.vdi.mipt.ru/ejudge/lab_01_mpl/frames.dat')
 Lines = url.text.split('\n')
 N = len(Lines)//2
 X, Y = [[float(j) for j in Lines[2*i].split()] for i in range(N)], [[float(j) for j in Lines[2*i + 1].split()] for i in range(N)]
+
+fig = plt.figure()
+
 frames = []
+#trans = plt.axes().transAxes
 
-def f(x, y):
-    return np.sin(x) + np.cos(y)
-
-x = np.linspace(0, 2 * np.pi, 120)
-y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
-# ims is a list of lists, each row is a list of artists to draw in the
-# current frame; here we are just animating one artist, the image, in
-# each frame
-ims = []
 for i in range(N):
-    x = X[i]
-    y = Y[i]
-    im = plt.plot(x,y, animated=True)
-    ims.append([im])
+    frame, = plt.plot(X[i], Y[i], 'r-')
+    frames.append([frame,])
 
-ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
-                                repeat_delay=1000)
+miny, maxy = min(Y[5]) - 0.1 * abs(max(Y[5]) - min(Y[5])), max(Y[5]) + 0.1 * abs(max(Y[5]) - min(Y[5]))
+plt.axis([min(X[0]), max(X[0]), miny, maxy])
+plt.grid(True)
 
-# To save the animation, use e.g.
-#
-# ani.save("movie.mp4")
-#
-# or
-#
-# from matplotlib.animation import FFMpegWriter
-# writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-# ani.save("movie.mp4", writer=writer)
-
+#ani = animation.ArtistAnimation(fig, lns, interval=50)
+anim = animation.ArtistAnimation(fig, frames, interval=200)
 plt.show()
