6bb74b8e5402a2807efff316700004f918d59965
KirikMaster <62561802+KirikMaster@users.noreply.github.com>
2020-12-02 08:03:44 +0300

    Add files via upload

---
+++ 3 sem/Numpy_labs/Episode_03.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import requests
+
+url = requests.get('http://judge2.vdi.mipt.ru/ejudge/lab_02_numpy/start.dat')
+Lines = [float(i) for i in url.text.split('\n')]
+u0 = np.array(Lines)
+N = len(u0)
+
+A = np.diag(np.ones(N)) + np.diag(np.ones(N-1) * (-1), k = - 1)
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 50), ylim=(0, 10))
+line, = ax.plot([], [], lw=3)
+x = np.linspace(1,N)
+
+def u(u0):
+    return u0 - 0.5 * A @ u0
+
+def init():
+    line.set_data(x,u0)
+    return line,
+
+def animate(i):
+    u1 = line.get_data()[1]
+    y = u(u1)
+    line.set_data(x,y)
+    return line,
+
+anim = FuncAnimation(fig, animate, init_func=init, frames=255, interval=10, blit=False)
+plt.show()
