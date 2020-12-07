546994cb241968f0ef8cdb38c23e87d240700ba6
KirikMaster <62561802+KirikMaster@users.noreply.github.com>
2020-12-08 02:47:44 +0300

    Update Episode_01.py

--- 3 sem/Pandas_labs/Episode_01.py
+++ 3 sem/Pandas_labs/Episode_01.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 import requests
 
-url = requests.get('http://judge2.vdi.mipt.ru/ejudge/lab_03_pandas/transactions.csv')
-trans = pd.read_csv('Pandas_labs/transactions.csv',index_col=0)
+trans = pd.read_csv('http://judge2.vdi.mipt.ru/ejudge/lab_03_pandas/transactions.csv',index_col=0)
 OK_trans = trans.loc[trans['STATUS'] == 'OK'].sort_values(by='SUM', ascending=False)
 OK_trans_Umbrella = OK_trans.loc[OK_trans['CONTRACTOR'] == 'Umbrella, Inc', 'SUM']
 
